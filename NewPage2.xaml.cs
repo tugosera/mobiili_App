a9aed2af21760329faa67a76fa8367debb9c51de
opilane <opilane@HP-CZC2349HT2>
2025-02-18 11:41:07 +0200

    :(

--- NewPage2.xaml.cs
+++ NewPage2.xaml.cs
@@ -2,8 +2,14 @@ namespace mobiili_App;
 
 public partial class NewPage2 : ContentPage
 {
-	public NewPage2()
-	{
-		InitializeComponent();
-	}
+    public NewPage2()
+    {
+        InitializeComponent();
+    }
+
+    void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
+    {
+
+        
+    }
 }
