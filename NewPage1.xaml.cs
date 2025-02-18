b8e22c90545b79bdab9621880418ba05b09d5f39
- <->
2025-02-18 10:01:20 +0200

    :>

--- NewPage1.xaml.cs
+++ NewPage1.xaml.cs
@@ -7,11 +7,19 @@ public partial class NewPage1 : ContentPage
 		InitializeComponent();
 	}
 
-    private async void OnClickedBtn3(object sender, EventArgs e)
+    bool i = false;
+    private void OnClickedBtn3(object sender, EventArgs e)
     {
-        while (true)
-        {
+        i = true;
+        green.BackgroundColor = Color.FromRgb(0, 255, 0);
+        red.BackgroundColor = Color.FromRgb(128, 128, 128);
+        Thread.Sleep(2000);
+        yellow.BackgroundColor = Color.FromRgb(255, 255, 0);
+        green.BackgroundColor = Color.FromRgb(128, 128, 128);
+        Thread.Sleep(2000);
+        red.BackgroundColor = Color.FromRgb(255, 0, 0);
+        yellow.BackgroundColor = Color.FromRgb(128, 128, 128);
+        Thread.Sleep(2000);
 
-        }
     }
 }  
