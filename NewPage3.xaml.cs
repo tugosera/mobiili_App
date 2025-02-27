263c2d96d2afd464013c17365a738ca630f45103
Erik Gerega <154507933+tugosera@users.noreply.github.com>
2025-02-27 14:06:23 +0200

    Add files via upload
    yippee

--- NewPage3.xaml.cs
+++ NewPage3.xaml.cs
@@ -1,5 +1,6 @@
-using Android.Content.Res;
+﻿using Android.Content.Res;
 using Kotlin.Jvm.Internal;
+using Microsoft.Maui.Controls;
 
 namespace mobiili_App;
 
@@ -32,5 +33,33 @@ public partial class NewPage3 : ContentPage
         head.BackgroundColor = Color.FromRgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
         body.BackgroundColor = Color.FromRgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
         legs.BackgroundColor = Color.FromRgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
+    }
+
+    void OnSliderValueChanged(object sender, ValueChangedEventArgs e)
+    {
+        meme.Opacity = 1 - e.NewValue;
+        Snowman.Opacity = e.NewValue;
+    }
+
+    private async void OnClickedBtnSalto(object sender, EventArgs e)
+    {
+        uint duration = 1000; 
+
+        await Task.WhenAll(
+            Snowman.RotateTo(90, duration / 4)
+        );
+        await Task.WhenAll(
+            Snowman.RotateTo(180, duration / 4)
+        );
+        await Task.WhenAll(
+            Snowman.RotateTo(270, duration / 4)
+        );
+        await Task.WhenAll(
+            Snowman.RotateTo(360, duration / 4)
+        );
+
+        Snowman.Rotation = 0;
+        Snowman.TranslationX = 0;
+        Snowman.TranslationY = 0;
     }
 }
